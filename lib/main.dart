771a97b4f746cf2897d4d65c7a7615ad8fd03c6c
JOKER <alaminmb808@gmail.com>
2024-10-30 01:16:08 +0600

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(clientApp() );
 }
@@ -13,73 +14,73 @@ class clientApp extends StatelessWidget{
     );
   }
 }
-class Home  extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-
-
-   return Scaffold(
-     appBar: AppBar(
-       title: Text('Welcome'),
-       titleSpacing: 40,
-       backgroundColor: Colors.transparent,
-       elevation: 10,
-       actions: [
-         IconButton(onPressed: (){print("Punch Approved");},
-             icon: Icon(Icons.notifications)
-         ),
-       ],
-     ),
-     bottomNavigationBar:BottomNavigationBar(
-     currentIndex: 0,
-       items: [
-         BottomNavigationBarItem(icon: Icon(Icons.dashboard),label: "Dashboard",backgroundColor: Colors.green),
-         BottomNavigationBarItem(icon: Icon(Icons.mouse_outlined),label: "Product",),
-         BottomNavigationBarItem(icon: Icon(Icons.percent),label: "Offers"),
-         BottomNavigationBarItem(icon: Icon(Icons.task),label: "Orders Details"),
-         BottomNavigationBarItem(icon: Icon(Icons.account_box),label: "Accounts"),
-       ],
-     // onTap: (int index){
-     //   if(index==0) {
-     //   }
-     //
-     //   if(index==1){
-     //
-     //   }
-     //
-     //   if(index==2){
-     //
-     //   }
-     //
-     //   if(index==3){
-     //
-     //   }
-     //
-     //   if(index==4){
-     //
-     //   }
-     //
-     // }
-   ) ,
-     drawer: Drawer(
-       child: ListView(
-         children: [
-           DrawerHeader(child:
-           Text("Hello MD.Almin")),
-           ListTile(title: Text("Dashboard")),
-           ListTile(title: Text("My Task")),
-           ListTile(title: Text("Market Scanning")),
-           ListTile(title: Text("Search")),
-           ListTile(title: Text("Attendence")),
-           ListTile(title: Text("My Assessment")),
-           ListTile(title: Text("Perfomance")),
-           ListTile(title: Text("Merchant Nearby")),
-           ListTile(title: Text("Activity Log")),
-           ListTile(title: Text("Log Out")),
-         ],
-       ) ,
-     ),
-     );
-  }
-}
+// class Home  extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//
+//
+//
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('Welcome'),
+//        titleSpacing: 40,
+//        backgroundColor: Colors.transparent,
+//        elevation: 10,
+//        actions: [
+//          IconButton(onPressed: (){print("Punch Approved");},
+//              icon: Icon(Icons.notifications)
+//          ),
+//        ],
+//      ),
+//      bottomNavigationBar:BottomNavigationBar(
+//      currentIndex: 0,
+//        items: [
+//          BottomNavigationBarItem(icon: Icon(Icons.dashboard),label: "Dashboard",backgroundColor: Colors.green),
+//          BottomNavigationBarItem(icon: Icon(Icons.mouse_outlined),label: "Product",),
+//          BottomNavigationBarItem(icon: Icon(Icons.percent),label: "Offers"),
+//          BottomNavigationBarItem(icon: Icon(Icons.task),label: "Orders Details"),
+//          BottomNavigationBarItem(icon: Icon(Icons.account_box),label: "Accounts"),
+//        ],
+//      // onTap: (int index){
+//      //   if(index==0) {
+//      //   }
+//      //
+//      //   if(index==1){
+//      //
+//      //   }
+//      //
+//      //   if(index==2){
+//      //
+//      //   }
+//      //
+//      //   if(index==3){
+//      //
+//      //   }
+//      //
+//      //   if(index==4){
+//      //
+//      //   }
+//      //
+//      // }
+//    ) ,
+//      drawer: Drawer(
+//        child: ListView(
+//          children: [
+//            DrawerHeader(child:
+//            Text("Hello MD.Almin")),
+//            ListTile(title: Text("Dashboard")),
+//            ListTile(title: Text("My Task")),
+//            ListTile(title: Text("Market Scanning")),
+//            ListTile(title: Text("Search")),
+//            ListTile(title: Text("Attendence")),
+//            ListTile(title: Text("My Assessment")),
+//            ListTile(title: Text("Perfomance")),
+//            ListTile(title: Text("Merchant Nearby")),
+//            ListTile(title: Text("Activity Log")),
+//            ListTile(title: Text("Log Out")),
+//          ],
+//        ) ,
+//      ),
+//      );
+//   }
+// }
--- page_lib/login_page.dart
+++ page_lib/login_page.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 
 import 'start_page.dart';
 
-void main (){
+void main() {
   runApp(CustoApp());
 }
-class CustoApp extends StatelessWidget{
+
+class CustoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,162 +17,274 @@ class CustoApp extends StatelessWidget{
     );
   }
 }
+
 class Home extends StatelessWidget {
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lets Part of your idea'),
-        titleTextStyle: TextStyle(color: Colors.white),
-        shape: RoundedRectangleBorder(
-          borderRadius:
-          BorderRadius.only(
-              bottomLeft: Radius.circular(15),
-              bottomRight: Radius.circular(15)
+        appBar: AppBar(
+          title: Text('Lets Part of your idea'),
+          titleTextStyle: TextStyle(color: Colors.white),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15)),
           ),
+          backgroundColor: Colors.black,
         ),
-        backgroundColor: Colors.black,
-      ),
-    body: Center(
-      child: Column(mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.asset('assets/innologo.jpg',
-          height: 200,
-          width: 200,
-          ),
-          Text('Login/Singup Modern IT Solution',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.black,
-          ),),
-      SizedBox(height: 10),
-          Padding(
-            padding:EdgeInsets.all(16),
-            child: TextField(
-              maxLength: 50,
-              onChanged: (String? value){
-                print(value);
-              },
-              controller: TextEditingController(),
-              maxLines: 1,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                // fillColor: Colors.blueAccent[100],
-                // filled: true,
-                counterText: '',
-                hintText: 'User Name or Email',
-                hintStyle: TextStyle(
-                  fontWeight: FontWeight.w300,
-                // color: Colors.black,
-                ),
-                labelText: 'User Name or Email',
-                // labelStyle: TextStyle(color: Colors.red[200]),
-                // label: Icon(Icons.person_2_rounded),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: BorderSide
-                  (
-                  color: Colors.deepPurple,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(
 
-                  color: Colors.deepPurple,
-                )
-              )
-              ),
-            ),
-          ),
-          SizedBox(height: 15),
-          Padding(
-            padding:EdgeInsets.all(16),
-            child: TextField(
-              obscureText: true,
-              maxLength: 50,
-              onChanged: (String? value){
-                print(value);
-              },
-              controller: TextEditingController(),
-              maxLines: 1,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                // fillColor: Colors.blueAccent[100],
-                // filled: true,
-                  counterText: '',
-                  hintText: 'Password',
-                  hintStyle: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    // color: Colors.black,
-                  ),
-                  labelText: 'Password',
-                  // labelStyle: TextStyle(color: Colors.red[200]),
-                  // label: Icon(Icons.person_2_rounded),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide
-                      (
-                      color: Colors.deepPurple,
+        // body:
+        // Center(
+        //   child:
+        //   Column(mainAxisAlignment: MainAxisAlignment.start,
+        //     children: [
+        //       Image.asset('assets/innologo.jpg',
+        //       height: 200,
+        //       width: 200,
+        //       ),
+        //       Text('Login/Singup Modern IT Solution',
+        //       style: TextStyle(
+        //         fontSize: 30,
+        //         color: Colors.black,
+        //       ),),
+        //
+        //   SizedBox(height: 10),
+        //
+        //       Padding(
+        //         padding:EdgeInsets.all(16),
+        //         child:
+        //         TextField(
+        //           maxLength: 50,
+        //           onChanged: (String? value){
+        //             print(value);
+        //           },
+        //           controller: TextEditingController(),
+        //           maxLines: 1,
+        //           keyboardType: TextInputType.emailAddress,
+        //           decoration: InputDecoration(
+        //             // fillColor: Colors.blueAccent[100],
+        //             // filled: true,
+        //             counterText: '',
+        //             hintText: 'User Name or Email',
+        //             hintStyle: TextStyle(
+        //               fontWeight: FontWeight.w300,
+        //             // color: Colors.black,
+        //             ),
+        //             labelText: 'User Name or Email',
+        //             // labelStyle: TextStyle(color: Colors.red[200]),
+        //             // label: Icon(Icons.person_2_rounded),
+        //           border: OutlineInputBorder(
+        //             borderRadius: BorderRadius.circular(15),
+        //             borderSide: BorderSide
+        //               (
+        //               color: Colors.deepPurple,
+        //             ),
+        //           ),
+        //           focusedBorder: OutlineInputBorder(
+        //               borderRadius: BorderRadius.circular(10),
+        //             borderSide: BorderSide(
+        //
+        //               color: Colors.deepPurple,
+        //             )
+        //           )
+        //           ),
+        //         ),
+        //       ),
+        //       SizedBox(height: 15),
+        //       Padding(
+        //         padding:EdgeInsets.all(16),
+        //         child: TextField(
+        //           obscureText: true,
+        //           maxLength: 50,
+        //           onChanged: (String? value){
+        //             print(value);
+        //           },
+        //           controller: TextEditingController(),
+        //           maxLines: 1,
+        //           keyboardType: TextInputType.emailAddress,
+        //           decoration: InputDecoration(
+        //             // fillColor: Colors.blueAccent[100],
+        //             // filled: true,
+        //               counterText: '',
+        //               hintText: 'Password',
+        //               hintStyle: TextStyle(
+        //                 fontWeight: FontWeight.w300,
+        //                 // color: Colors.black,
+        //               ),
+        //               labelText: 'Password',
+        //               // labelStyle: TextStyle(color: Colors.red[200]),
+        //               // label: Icon(Icons.person_2_rounded),
+        //               border: OutlineInputBorder(
+        //                 borderRadius: BorderRadius.circular(15),
+        //                 borderSide: BorderSide
+        //                   (
+        //                   color: Colors.deepPurple,
+        //                 ),
+        //               ),
+        //               focusedBorder: OutlineInputBorder(
+        //                   borderRadius: BorderRadius.circular(8),
+        //                   borderSide: BorderSide(
+        //
+        //                     color: Colors.deepPurple,
+        //                   )
+        //               )
+        //           ),
+        //         ),
+        //       ),
+        //
+        //     ElevatedButton(
+        //         onPressed: (){},
+        //         child:
+        //         Text('Login'),
+        //       style: ElevatedButton.styleFrom(
+        //         textStyle: TextStyle(
+        //         color: Colors.black,
+        //           // color: Colors.white,
+        //           fontSize: 15,),
+        //         backgroundColor: Colors.white,
+        //         shape: RoundedRectangleBorder(
+        //           borderRadius: BorderRadius.circular(25),
+        //         )
+        //       ),
+        //     ),
+        //       SizedBox(
+        //         height: 15,
+        //       ),
+        //       ElevatedButton(onPressed: (){},
+        //           child:
+        //         Text('Sign up'),
+        //         style: ElevatedButton.styleFrom(
+        //          const  textStyle: TextStyle(
+        //             fontSize: 15,
+        //
+        //           )
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter Your Email or Phone No';
+                  }
+                  return null;
+                },
+                controller: _emailTextController,
+                decoration: InputDecoration(
+                    counterText: '',
+                    hintText: 'User Name or Email',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      // color: Colors.black,
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
+                    labelText: 'User Name or Email',
+                    // labelStyle: TextStyle(color: Colors.red[200]),
+                    // label: Icon(Icons.person_2_rounded),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
                       borderSide: BorderSide(
-
                         color: Colors.deepPurple,
-                      )
-                  )
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Colors.deepPurple,
+                        ))),
               ),
-            ),
-          ),
-        ElevatedButton(
-            onPressed: (){},
-            child: 
-            Text('Login'),
-          style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(
-            color: Colors.black,
-              // color: Colors.white,
-              fontSize: 15,),
-            backgroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25),
-            )
+              SizedBox(height: 15),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter Your Password';
+                  }
+                  return null;
+                },
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    counterText: '',
+
+
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      // color: Colors.black,
+                    ),
+                    labelText: 'Password',
+                    // labelStyle: TextStyle(color: Colors.red[200]),
+                    // label: Icon(Icons.person_2_rounded),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(
+                          color: Colors.deepPurple,
+                        ))),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // String email=_emailTextController.text;
+                  // String password=_passwordController.text;
+                  // if (email.isNotEmpty && password.isNotEmpty){
+                  //   print('Log In Successful');
+                  // }
+                  // else{
+                  //   print('Data Not valid');
+                  // }
+                  if (_formKey.currentState!.validate()) {
+                    print('Log in Successful');
+                  }
+                },
+                child: Text('Login'),
+                style: ElevatedButton.styleFrom(
+                    textStyle: TextStyle(
+                      color: Colors.black,
+                      // color: Colors.white,
+                      fontSize: 15,
+                    ),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    )),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Sign up'),
+                style: ElevatedButton.styleFrom(
+                    textStyle: TextStyle(
+                  fontSize: 15,
+                )),
+              ),
+            ],
           ),
         ),
-          SizedBox(
-            height: 15,
-          ),
-          ElevatedButton(onPressed: (){},
-              child:
-            Text('Sign up'),
-            style: ElevatedButton.styleFrom(
-              textStyle: TextStyle(
-                fontSize: 15,
-
-              )
-            ),
-          ),
-        ],
-      ),
-    ),
-
-      bottomNavigationBar: NavigationBar(
-        height: 90,
+        bottomNavigationBar: NavigationBar(
+          height: 90,
           backgroundColor: Colors.black,
           selectedIndex: 1,
-          destinations:[
-            NavigationDestination(
-                icon: Icon(Icons.web),label: "website"),
-            NavigationDestination(
-                icon: Icon(Icons.percent),label: "Offers"),
-            NavigationDestination(
-                icon: Icon(Icons.call),label: "helpline"),
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.web), label: "website"),
+            NavigationDestination(icon: Icon(Icons.percent), label: "Offers"),
+            NavigationDestination(icon: Icon(Icons.call), label: "helpline"),
           ],
-      ),
-
-
-    );
+        ));
   }
 }
